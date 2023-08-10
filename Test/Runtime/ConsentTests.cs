31a0508ad6992f977525f803d111634b4289b07c
SCastanedaMunoz <santiagocm98@hotmail.com>
2023-08-09 19:29:41 -0500

    Updating Unit Tests

--- Test/Runtime/ConsentTests.cs
+++ Test/Runtime/ConsentTests.cs
@@ -10,24 +10,20 @@ namespace Chartboost.Core.Usercentrics.Tests
     public class ConsentTests 
     {
         private const string DateFormat = "yyyy/MM/dd HH:mm:ss.fff";
-        private const float ConstDelayAfterInit = 0.5f;
+        private const float ConstDelayAfterInit = 2f;
         
-        private static UsercentricsOptions usercentricsOptions = new UsercentricsOptions("");
+        private static readonly UsercentricsOptions UsercentricsOptions = new UsercentricsOptions("DZbpqFbm-bHtwC");
         
-        private static readonly UsercentricsAdapter UsercentricsAdapter = new UsercentricsAdapter("ChartboostCore", usercentricsOptions);
+        private static readonly UsercentricsAdapter UsercentricsAdapter = new UsercentricsAdapter("ChartboostCore", UsercentricsOptions);
         
         private readonly InitializableModule[] _modules = {
             UsercentricsAdapter
         };
-        
+
         [SetUp]
         public void Setup()
         {
             ChartboostCore.Debug = true;
-            ChartboostCore.Consent.ConsentStatusChange += status =>
-            {
-                ChartboostCoreLogger.Log($"SCM: STATUS CHANGED: {status}");
-            };
         }
 
         [UnityTest, Order(3)]
@@ -49,7 +45,7 @@ namespace Chartboost.Core.Usercentrics.Tests
                 Assert.AreEqual(result.Module.ModuleId, UsercentricsAdapter.ModuleId);
                 Assert.AreEqual(result.Module.ModuleVersion, UsercentricsAdapter.ModuleVersion);
 
-                var settingsId = usercentricsOptions.SettingsId;
+                var settingsId = UsercentricsOptions.SettingsId;
 
                 if (string.IsNullOrEmpty(settingsId))
                     ChartboostCoreLogger.Log($"Exception: {result.Exception?.Message}");
@@ -69,8 +65,11 @@ namespace Chartboost.Core.Usercentrics.Tests
             ChartboostCore.Initialize(sdkConfig, _modules);
             yield return new WaitForSeconds(ConstDelayAfterInit);
             ChartboostCore.ModuleInitializationCompleted -= AssertModule;
+
+            var task = ChartboostCore.Consent.SetConsentStatus(ConsentStatus.Unknown, ConsentStatusSource.Developer);
+            yield return new WaitUntil(() => task.IsCompleted);
         }
-        
+
         [UnityTest, Order(4)]
         public IEnumerator SetConsentStatusGranted()
         {
@@ -91,30 +90,16 @@ namespace Chartboost.Core.Usercentrics.Tests
         
         private static IEnumerator TestStatus(ConsentStatus status)
         {
-            var callback = false;
-            ChartboostCore.Consent.ConsentStatusChange += AssertStatus;
             ChartboostCoreLogger.Log($"Initial - Target Status: {status}, Current Status {ChartboostCore.Consent.ConsentStatus}");
             
             var task = ChartboostCore.Consent.SetConsentStatus(status, ConsentStatusSource.Developer);
             
             yield return new WaitUntil(() => task.IsCompleted);
             
-            ChartboostCoreLogger.Log($"SCM - Result: {task.Result}");
-            
-            if (task.Result)
-                yield return new WaitUntil(() => callback);
-
             var currentStatus = ChartboostCore.Consent.ConsentStatus;
             ChartboostCoreLogger.Log($"After - Target Status: {status}, Current Status {currentStatus}");
             Assert.IsTrue(task.Result);
-
-            void AssertStatus(ConsentStatus newStatus)
-            {
-                // Assert.AreEqual(newStatus, status);
-                ChartboostCoreLogger.Log($"Callback - Target Status: {status}, New Status: {newStatus}");
-                ChartboostCore.Consent.ConsentStatusChange -= AssertStatus;
-                callback = true;
-            }
+            Assert.AreEqual(status, currentStatus);
         }
     }
 }
